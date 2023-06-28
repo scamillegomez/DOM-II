0e28de04e30d907962769740c82a09750988ee2d
scgomez <sydneycamille0896@gmail.com>
2023-06-27 20:27:36 -0500

    submitting with passing npm test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,71 @@
 import './less/index.less'
 
-const headerText = document.querySelector('.logo-heading');
+// const heading = document.querySelector('')
+
 
 // event #1
 window.addEventListener('load',(event) => {
     alert('Fun Bus has arrived!');
+    let headerText = document.querySelector('.logo-heading'); 
+    let welcomeText = document.querySelector('.intro p');
+    welcomeText.addEventListener('dblclick',function(e){
+        welcomeText.style="background-color:darkgrey;color:white;font-style:bold;border:1px solid darkgrey;border-radius:15px;padding:1%"});
+    const imgParent = document.querySelector('.intro');
+    const imgActual = document.querySelector('img:nth-of-type(1)');
+    
+    headerText.addEventListener('mouseover', () => {
+        headerText.style = "fontFamily: 'Arial, sans-serif'; color:pink";
+    });
+    headerText.addEventListener('mouseleave', event => event.target.style ='default');
+
+    let dropZone = document.createElement('div');
+    dropZone.id = 'dropZone';
+    dropZone.style.backgroundColor = 'lightblue';
+    dropZone.style.height = '170px';
+    dropZone.style.width = '800px';
+    dropZone.style.border = '1px solid lightblue';
+    dropZone.innerText = 'drop here';
+    dropZone.style.position = 'relative';
+    // dropZone.style.top = '100px';
+
+    imgParent.replaceChild(dropZone,imgActual);
+
+    var headerBar = document.querySelector('.main-navigation');
+    headerBar.appendChild(draggable);
+
+    //event #9
+    dropZone.addEventListener('dragover',function(e){
+    e.preventDefault();
+    });
+
+    //event #10
+    dropZone.addEventListener('drop',function(e){
+    e.preventDefault();
+    var data = e.dataTransfer.getData('text');
+
+    var draggableItem = document.getElementById(data);
+
+    var dropPositionX = e.clientX - dropZone.getBoundingClientRect().left;
+    var dropPositionY = e.clientY - dropZone.getBoundingClientRect().top;
+
+    draggableItem.style.position = 'absolute';
+    draggableItem.style.left = `${dropPositionX}px`;
+    draggableItem.style.top = `${dropPositionY}px`;
+
+    e.target.appendChild(draggableItem);
+
+
 });
 
-const exagText = function(){
-    headerText.style = "fontFamily: 'Arial, sans-serif'; color:pink";
-}
+    dropZone.insertAdjacentElement('afterend',button);
 
-// event #2
-headerText.addEventListener('mouseover',exagText);
+});
 
-// event #3
-headerText.addEventListener('mouseleave', event=>event.target.style ='default');
+
+// const exagText = function(){
+//     headerText.style = "fontFamily: 'Arial, sans-serif'; color:pink";
+// }
+
 
 let busDiv = document.createElement('div');
 busDiv.style.position = 'absolute';
@@ -42,6 +92,7 @@ document.addEventListener('mousemove',function(e){
 
 const navBar = document.querySelector('.main-navigation');
 const aLinks = document.querySelectorAll('a');
+
 document.addEventListener('wheel',function(e){
     navBar.style = "background-color:#227aa1;color:white;";
     aLinks.forEach(el=>el.style='color:white');
@@ -50,26 +101,7 @@ document.addEventListener('wheel',function(e){
 
 //event #6
 
-const welcomeText = document.querySelector('.intro p');
-welcomeText.addEventListener('dblclick',function(e){
-    welcomeText.style="background-color:darkgrey;color:white;font-style:bold;border:1px solid darkgrey;border-radius:15px;padding:1%"});
-
-//event #7
-
-const imgParent = document.querySelector('.intro');
-const imgActual = document.querySelector('img:nth-of-type(1)');
-
-let dropZone = document.createElement('div');
-dropZone.id = 'dropZone';
-dropZone.style.backgroundColor = 'lightblue';
-dropZone.style.height = '170px';
-dropZone.style.width = '800px';
-dropZone.style.border = '1px solid lightblue';
-dropZone.innerText = 'drop here';
-dropZone.style.position = 'relative';
-// dropZone.style.top = '100px';
-
-imgParent.replaceChild(dropZone,imgActual);
+// const welcomeText = document.querySelector('p');
 
 var draggable = document.createElement('span');
 draggable.id = 'draggable';
@@ -77,45 +109,21 @@ draggable.innerText = '🚌';
 draggable.style.fontSize = '50px';
 draggable.setAttribute('draggable','true');
 
-var headerBar = document.querySelector('.main-navigation');
-headerBar.appendChild(draggable);
+// var headerBar = document.querySelector('.main-navigation');
+
 
 //event #8
 draggable.addEventListener('dragstart',function(e){
     e.dataTransfer.setData('text',e.target.id);
 });
 
-//event #9
-dropZone.addEventListener('dragover',function(e){
-    e.preventDefault();
-});
-
-//event #10
-dropZone.addEventListener('drop',function(e){
-    e.preventDefault();
-    var data = e.dataTransfer.getData('text');
-
-    var draggableItem = document.getElementById(data);
-
-    var dropPositionX = e.clientX - dropZone.getBoundingClientRect().left;
-    var dropPositionY = e.clientY - dropZone.getBoundingClientRect().top;
-
-    draggableItem.style.position = 'absolute';
-    draggableItem.style.left = `${dropPositionX}px`;
-    draggableItem.style.top = `${dropPositionY}px`;
-
-    e.target.appendChild(draggableItem);
-
-
-});
-
 
 var button = document.createElement('button');
     button.innerText = 'Export';
 
-    dropZone.insertAdjacentElement('afterend',button);
+    
 
         //bonus event! 
-        button.addEventListener('click',function(){
-            alert('you win!');
-        })
+button.addEventListener('click',function(){
+alert('you win!');
+        });
